171dcf6812acb1c42cdd55c98036c2c488cc574a
eulincjay53-spec <angelyn.carias-25@cpu.edu.ph>
2026-04-23 18:11:54 +0800

    added finals files
    This is my finals laboratory exam

---
+++ finals/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:finals/screen/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: "https://pnjxydzavtvlnhrerdku.supabase.co", 
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuanh5ZHphdnR2bG5ocmVyZGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODQ2NTQsImV4cCI6MjA4NjQ2MDY1NH0.IkULjAQlDZ7yntiQ81nd9oGL2GP4v3LouYq5sgQNBm0"
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Live Chat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      home: const ChatPage(username: 'Angelyn',), 
+    );
+  }
+}
